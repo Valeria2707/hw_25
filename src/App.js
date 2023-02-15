bb2225bc6293f945394d059fa56f4ed1d82a6079
Valeria Zavadetska <Valeria.zavadetska@gmail.com>
2023-02-15 12:00:46 +0200

    calculator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,86 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import {btn, btnOperation} from './buttonData';
 
 function App() {
+  let [value, setValue] = useState('');
+  let [result, setResult] = useState(0);
+  let [operations, setOperations] = useState('+');
+  let [showResult, setShowResult] = useState(false);
+
+  const showValue = (event) =>{
+    const number = event.target.innerHTML
+    const bigNumber = value.concat(number);
+    console.log(bigNumber)
+    setValue(bigNumber);
+  }
+
+   const clickOperations = (event) =>{
+    setShowResult(false);
+    const operation = event.target.innerHTML
+    console.log(operation);
+    setOperations(operation)
+    setValue('');
+    const values = Number(value);
+    setResult(values)
+  }
+
+  const showRes = () =>{
+    setShowResult(true)
+    const values = Number(value);
+    if(operations === '+')
+    {
+      setResult(result += values)
+      console.log(result)
+    }
+    else if(operations === '-')
+    {
+      setResult(result -= values)
+      console.log(result)
+    }
+    else if(operations === 'x')
+    {
+      setResult(result *= values)
+      console.log(result)
+    }
+    else if(operations === '/')
+    {
+      setResult(result /= values)
+      console.log(result)
+    }
+    else if(operations === '%')
+    {
+      setResult(result /= 100)
+      console.log(result)
+    }
+    else if(operations === '√')
+    {
+      setResult(Math.sqrt(result))
+      console.log(result)
+    }
+    else if(operations === 'С'){
+      setShowResult(false);
+      result = ''
+      console.log(result)
+    }
+
+    setValue(String(result));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+      <div className='text'>{showResult ? result : value}</div>
+      <div className='keypad'>
+        {btnOperation.map(button =>{
+           return <button key={button.id} onClick={clickOperations}  className='option'>{button.value}</button>
+        })}
+        {btn.map(button =>{
+           return <button key={button.id} onClick={showValue}>{button.value}</button>
+        })}
+        <button className='res option' onClick={showRes}>=</button>
+      </div>
+      </div>
     </div>
   );
 }
---
+++ src/buttonData.js
@@ -0,0 +1,73 @@
+export const btn = [
+    {
+      id: 0,
+      value: '9'
+    },
+    {
+      id: 1,
+      value: '5'
+    },
+    {
+      id: 2,
+      value: '6'
+    },
+    {
+      id: 3,
+      value: '7'
+    },
+    {
+      id: 4,
+      value: '8'
+    },
+    {
+      id: 5,
+      value: '1'
+    },
+    {
+      id: 6,
+      value: '2'
+    },
+    {
+      id: 7,
+      value: '3'
+    },
+    {
+      id: 8,
+      value: '4'
+    },
+    {
+      id: 9,
+      value: '0'
+    },
+  ]
+
+export const btnOperation = [
+  {
+    id: 11,
+    value: '+'
+  },
+  {
+    id: 12,
+    value: '-'
+  },
+  {
+    id: 13,
+    value: 'x'
+  },
+  {
+    id: 14,
+    value: '/'
+  },
+  {
+    id: 15,
+    value: '%'
+  },
+  {
+    id: 16,
+    value: '√'
+  },
+  {
+    id: 17,
+    value: 'С'
+  },
+]
